34c0379156a0daec1559b31ff26d5262fe6fa7e6
Brian Downs <brian.downs@gmail.com>
2022-07-26 16:27:23 -0700

    update build script
    Signed-off-by: Brian Downs <brian.downs@gmail.com>

--- Makefile
+++ Makefile
@@ -1,17 +1,13 @@
 .PHONY: ent-k3s
 ent-k3s:
-	cd ${GOPATH}/src/github.com/briandowns/k3s
-	PROG=lke IMAGE_NAME=lke SKIP_VALIDATE=true make
+	./k3s_build.sh
+
+.PHONY: ent-rancher
+ent-rancher:
+	cd ${GOPATH}/src/github.com/briandowns/rancher
+	./rancher_build.sh
 
 .PHONY: clean
 clean:
 	rm -rf ./k3s
-
-# rename files
-# - k3s.service
-# k3s-rootless.service
-#
-# Does kubectl get nodes output matter if it has k3s in it.
-#
-# lke binary dist/artifacts/lke size 67350528 exceeds max acceptable size of 67108864 bytes (64 MiB) - 241664 diff
-#
+	rm -rf ./rancher
--- k3s_build.sh
+++ k3s_build.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
+# Does kubectl get nodes output matter if it has k3s in it.
+#
+# lke binary dist/artifacts/lke size 67350528 exceeds max acceptable size of 67108864 bytes (64 MiB) - 241664 diff
+
 set -e
 
 if [ "${DEBUG}" ]; then
     set -x
 fi
 
-PROG=${PROG:=lke}
-BUILD_IMAGE=""
+PROG="lke"
 SKIP_VALIDATE=${SKIP_VALIDATE:=false}
-
-if [ ${FIPS} ]; then
-    BUILD_IMAGE=rancher/hardened-build-base:v1.18.4b7
-fi
+FIPS_BUILD_IMAGE=rancher/hardened-build-base:v1.18.1b7
 
 # update_service_files renames the k3s systemd service files
 # and updates the content to reflect the name of the enterprise
@@ -22,19 +22,30 @@ update_service_files() {
     local rootless_service_file="${PROG}-rootless.service"
 
     mv k3s.service "${service_file}"
-    sed -i 's/k3s/${PROG}/g' "${service_file}"
+    sed -i "s/k3s/${PROG}/g" "${service_file}"
 
     mv k3s-rootless.service "${rootless_service_file}"
-    sed -i 's/k3s/${PROG}/g' "${rootless_service_file}"
+    sed -i "s/k3s/${PROG}/g" "${rootless_service_file}"
 }
 
-# git clone git@github.com:k3s-io/k3s.git
-cd ${GOPATH}/src/github.com/briandowns/k3s
+# update_manifests updates the manifests to use the ${PROG}
+# name for users, etc... (ccm & rolebindings)
+update_manifests() {
+    sed -i "s/k3s/${PROG}/g" manifests/ccm.yaml
+    sed -i "s/k3s/${PROG}/g" manifests/rolebindings.yaml
+}
 
-if [ "${BUILD_IMAGE}" != "" ]; then
-    PROG=${PROG} IMAGE_NAME=${PROG} SKIP_VALIDATE=${SKIP_VALIDATE} GOLANG=${BUILD_IMAGE} make
+git clone git@github.com:k3s-io/k3s.git
+
+cd k3s
+
+if [ ${FIPS} ]; then
+    PROG=${PROG} SKIP_VALIDATE=${SKIP_VALIDATE} GOLANG=${FIPS_BUILD_IMAGE} make
 else
-    PROG=${PROG} IMAGE_NAME=${PROG} SKIP_VALIDATE=${SKIP_VALIDATE} make
+    PROG=${PROG} SKIP_VALIDATE=${SKIP_VALIDATE} make
 fi
+
+update_service_files
+update_manifests
 
 exit 0
